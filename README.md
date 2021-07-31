a95f8cb3e9c6691b274a16283d4a73120807be6a
Tim Kleyersburg <tk@wacg.de>
2021-07-31 21:11:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Last tweet of @timkley
+
+This worker get's the last tweet if @timkley as a JSON
+response using a Cloudflare Worker and the Twitter API.
---
+++ index.js
@@ -0,0 +1,36 @@
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+    const oauthToken = await getOauthToken();
+
+    const response = await fetch('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=timkley&count=1', {
+        headers: {
+            'Authorization': `Bearer ${oauthToken}`
+        }
+    })
+
+    return new Response(JSON.stringify(await response.json()))
+}
+
+async function getOauthToken() {
+    const basicAuth = btoa(`${TWITTER_API_KEY}:${TWITTER_API_SECRET}`)
+
+    const response = await fetch('https://api.twitter.com/oauth2/token', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Basic ${basicAuth}`,
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        body: 'grant_type=client_credentials'
+    })
+
+    const json = await response.json()
+
+    return json['access_token']
+}

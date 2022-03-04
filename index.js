2343df9b391007699cfdc21e933e073a1092ece7
Tim Kleyersburg <tk@wacg.de>
2022-03-04 13:03:49 +0100

    chore: formatting

--- index.js
+++ index.js
@@ -6,18 +6,22 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-    const oauthToken = await getOauthToken();
+    const cache = caches.default
+    const oauthToken = await getOauthToken()
 
-    const response = await fetch('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=timkley&count=1', {
-        headers: {
-            'Authorization': `Bearer ${oauthToken}`
-        }
-    })
+    const response = await fetch(
+        'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=timkley&count=1',
+        {
+            headers: {
+                Authorization: `Bearer ${oauthToken}`,
+            },
+        },
+    )
 
     return new Response(JSON.stringify(await response.json()), {
         headers: {
-            'Access-Control-Allow-Origin': '*'
-        }
+            'Access-Control-Allow-Origin': '*',
+        },
     })
 }
 
@@ -27,10 +31,10 @@ async function getOauthToken() {
     const response = await fetch('https://api.twitter.com/oauth2/token', {
         method: 'POST',
         headers: {
-            'Authorization': `Basic ${basicAuth}`,
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            Authorization: `Basic ${basicAuth}`,
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
         },
-        body: 'grant_type=client_credentials'
+        body: 'grant_type=client_credentials',
     })
 
     const json = await response.json()
